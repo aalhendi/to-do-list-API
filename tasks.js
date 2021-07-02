708c79d65d56b63fb9dd51ddf2e6a57e87be302d
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-07-02 18:35:01 +0300

    Add priority to tasks

--- tasks.js
+++ tasks.js
@@ -4,24 +4,28 @@ const tasks = [
     name: "Task 1",
     description: "A simple Task",
     status: true,
+    priority: "low",
   },
   {
     id: 2,
     name: "Task 2",
     description: "A simple Task",
     status: false,
+    priority: "low",
   },
   {
     id: 3,
     name: "Task 3",
     description: "A simple Task",
     status: false,
+    priority: "low",
   },
   {
     id: 4,
     name: "Task 4",
     description: "A simple Task",
     status: false,
+    priority: "low",
   },
 ];
 
